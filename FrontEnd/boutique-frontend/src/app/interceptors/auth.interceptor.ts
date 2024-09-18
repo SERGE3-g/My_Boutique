5d3cf4319aeca8bf3064d98d2ca20820124220bf
SERGE3-g <gueaserge2@gmail.com>
2024-09-18 02:12:50 +0200

    new jwt

--- FrontEnd/boutique-frontend/src/app/interceptors/auth.interceptor.ts
+++ FrontEnd/boutique-frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,27 +1,27 @@
+// src/app/interceptors/auth.interceptor.ts
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root', // Assicurati che il servizio sia iniettato correttamente
+})
 export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Recupera il token JWT dal servizio di autenticazione
     const token = localStorage.getItem('token');
 
-    // Clona la richiesta e aggiungi l'intestazione Authorization se il token è presente
     if (token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
     }
 
-    // Passa la richiesta modificata al prossimo gestore (handler)
     return next.handle(request);
   }
 }
